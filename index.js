9bf78e988b8709604450ad806a7b50b4b7f649d7
TracyAntonia <aantonjuguna@gmail.com>
2023-06-22 22:26:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return (add)
+}
+function subtract(a, b) {
+    return (subtract)
+}
+function multiply(a, b) {
+    return (multiply)
+}
+function divide(a, b) {
+    return (divide)
+}
+function add(a, b) {
+    return (a + b)
+}
+function subtract(a, b) {
+    return (a - b)
+}
+function multiply(a, b) {
+    return (a * b)
+}
+function divide(a, b) {
+    return (a / b)
+}
+function increment(a, n){
+    n = 1;
+    a++;
+    return a;
+}
+function decrement(a, n){
+    n = 1;
+    a--;
+    return a;
+}
+function makeInt(a){
+    return parseInt(a, 10);
+}
+function preserveDecimal(a){
+    return parseFloat(a);
+}
